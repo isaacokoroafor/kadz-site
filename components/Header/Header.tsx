4a8d33a85de940fdef21cb632ef47e00cffd6d0d
isaacokoroafor <52671538+isaacokoroafor@users.noreply.github.com>
2024-03-10 17:46:13 +0000

    fixes 1

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,44 +4,53 @@ import { MainNav } from "./MainNav";
 
 const Header = () => {
   return (
-    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify between">
-      <div className="flex items-center w-full md:2/3">
-        <ul className="flex items-center ml-5"></ul>
-        <Link href="/menu">
-          <div className="h-9 px-3 rounded-md hover:bg-accent hover:text-accent-foreground">
-            <Menu className="h-5 w-5" />
-            <span className="sr-only">Cart</span>
-          </div>
-        </Link>
-        <Link href="/search">
-          <div className="h-9 px-3 rounded-md hover:bg-accent hover:text-accent-foreground">
-            <Search className="h-5 w-5" />
-            {/* <span className="ml-2 text-sm font-bold">0</span> */}
-            <span className="sr-only">Search</span>
-          </div>
-        </Link>
-
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0 md:2/3">
+        <ul className="flex items-center space-x-1">
+          <li>
+            <Link href="/menu">
+              <div className="px-3 rounded-md hover:bg-accent hover:text-accent-foreground">
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Cart</span>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="/search">
+              <div className="px-3 rounded-md hover:bg-accent hover:text-accent-foreground">
+                <Search className="h-5 w-5" />
+                {/* <span className="ml-2 text-sm font-bold">0</span> */}
+                <span className="sr-only">Search</span>
+              </div>
+            </Link>
+          </li>
+        </ul>
+        {/* site logo on navbar */}
         <div className="absolute right-1/2 translate-x-1/2 translate-y-4">
           <MainNav />
         </div>
 
-        <div className="flex items-center space-x-1">
-          <Link href="/shopping">
-            <div className="h-9 px-3 rounded-md hover:bg-accent hover:text-accent-foreground">
-              <ShoppingBag className="h-5 w-5" />
-              <span className="ml-2 text-sm font-bold">0</span>
-              <span className="sr-only">Cart</span>
-            </div>
-          </Link>
-          {/* <ThemeToggle /> */}
-          {process.env.NODE_ENV === "development" && (
-            <Link href="/studio">
-              <div className="h-9 px-3 rounded-md hover:bg-accent hover:text-accent-foreground">
-                <Edit className="h-5 w-5" />
+        <ul className="flex items-center space-x-1">
+          <li>
+            <Link href="/shopping">
+              <div className="px-3 rounded-md hover:bg-accent hover:text-accent-foreground">
+                <ShoppingBag className="h-5 w-5" />
+                <span className="sr-only">Cart</span>
               </div>
             </Link>
-          )}
-        </div>
+          </li>
+
+          {/* <ThemeToggle /> */}
+          <li>
+            {process.env.NODE_ENV === "development" && (
+              <Link href="/studio">
+                <div className=" px-3 rounded-md hover:bg-accent hover:text-accent-foreground">
+                  <Edit className="h-5 w-5" />
+                </div>
+              </Link>
+            )}
+          </li>
+        </ul>
       </div>
     </header>
   );
